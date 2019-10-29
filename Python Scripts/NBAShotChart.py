d67e621d2b0b2ee98808c7a1929a1d8fb62f82fa
kloudKlown <suhas.servesh@gmail.com>
2019-10-29 12:49:12 -0400

    Shot Chart Script Update

--- Python Scripts/NBAShotChart.py
+++ Python Scripts/NBAShotChart.py
@@ -6,28 +6,17 @@ import scrapy
 from scrapy import Request
 from scrapy.spiders.crawl import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-sys.path.append('../')
-from items import JobItem
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 import pyodbc
 import hashlib
 import time 
 import json
 
-
-def main():
-    co = Options()
-    driver = webdriver.Chrome( options = co, executable_path = r"D:\\NBA\\NBA\\spiders\\chromedriver.exe")
-    driver.get("https://stats.nba.com/players/list/")
-    time.sleep(5)
-    html = driver.page_source
-    soup = BeautifulSoup(html, features= 'html5lib', from_encoding= 'utf-8')
-    positions = {}
-    team = { 
+team = { 
         "Atlanta Hawks" : "ATL",
             "Boston Celtics" : "BOS",
             "Brooklyn Nets" : "BRK",
@@ -58,15 +47,23 @@ def main():
             "Toronto Raptors" : "TOR",
             "Utah Jazz" : "UTA",
             "Washington Wizards" : "WAS"
-    }
+}
 
-    hrefs = soup.findAll("li", {"class" : "players-list__name"})
+def main():
+    co = Options()
+    driver = webdriver.Chrome( options = co, executable_path = r"D:\\DFS Website\\DFS\\Python Scripts\\chromedriver.exe")
+    driver.get("https://stats.nba.com/players/list/")
+    time.sleep(5)
+    html = driver.page_source
+    soup = BeautifulSoup(html)
+    positions = {}
+    hrefs = soup.findAll("li", {"class" : "players-list__name"})    
     for href in hrefs:        
         if "href=" in str(href.find('a')) and re.match("/player/.*", href.find('a')["href"]):            
             id = str(href.find('a')['href'])
             id = id.split('/')[-2]            
             if ( len(id) > 3 ):
-                href = "https://stats.nba.com/stats/shotchartdetail?AheadBehind=&CFID=33&CFPARAMS=2018-19&ClutchTime=&Conference=&ContextFilter=&ContextMeasure=FGA&DateFrom=&DateTo=&Division=&EndPeriod=10&EndRange=28800&GROUP_ID=&GameEventID=&GameID=&GameSegment=&GroupID=&GroupMode=&GroupQuantity=5&LastNGames=0&LeagueID=00&Location=&Month=0&OnOff=&OpponentTeamID=0&Outcome=&PORound=0&Period=0&PlayerID=" + id + "&PlayerID1=&PlayerID2=&PlayerID3=&PlayerID4=&PlayerID5=&PlayerPosition=&PointDiff=&Position=&RangeType=0&RookieYear=&Season=2018-19&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StartPeriod=1&StartRange=0&StarterBench=&TeamID=0&VsConference=&VsDivision=&VsPlayerID1=&VsPlayerID2=&VsPlayerID3=&VsPlayerID4=&VsPlayerID5=&VsTeamID="
+                href = "https://stats.nba.com/stats/shotchartdetail?AheadBehind=&CFID=33&CFPARAMS=2019-20&ClutchTime=&Conference=&ContextFilter=&ContextMeasure=FGA&DateFrom=&DateTo=&Division=&EndPeriod=10&EndRange=28800&GROUP_ID=&GameEventID=&GameID=&GameSegment=&GroupID=&GroupMode=&GroupQuantity=5&LastNGames=0&LeagueID=00&Location=&Month=0&OnOff=&OpponentTeamID=0&Outcome=&PORound=0&Period=0&PlayerID=" + id + "&PlayerID1=&PlayerID2=&PlayerID3=&PlayerID4=&PlayerID5=&PlayerPosition=&PointDiff=&Position=&RangeType=0&RookieYear=&Season=2019-20&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StartPeriod=1&StartRange=0&StarterBench=&TeamID=0&VsConference=&VsDivision=&VsPlayerID1=&VsPlayerID2=&VsPlayerID3=&VsPlayerID4=&VsPlayerID5=&VsTeamID="
                 driver.get(href)
                 time.sleep(3)
                 soup = BeautifulSoup(driver.page_source)
